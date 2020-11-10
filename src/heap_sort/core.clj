e06e6e8e96532bc5ce79559822d14a866709ae6a
ahmedtarek- <ahmed.t.abdelfattah@gmail.com>
2020-11-10 02:07:15 +0100

    Sorting working and gurantees max is at top

--- src/heap_sort/core.clj
+++ src/heap_sort/core.clj
@@ -15,7 +15,6 @@
 
 (defn insert
   [value node]
-  (println value "     " node)
   (if (= node nil)
     {:value value :left nil :right nil}
     (if (<= (size (:left node)) (size (:right node)))
@@ -26,14 +25,57 @@
   [list-of-stuff]
   (def node nil)
   (doseq [x list-of-stuff]
-    (def node (insert x node))
-    (println "      ")
-    (println node))
-  )
+    (def node (insert x node)))
+  node)
+
+(defn treat
+  [element]
+  (if (= element nil)
+    ##-Inf
+    element))
+
+(defn max-with-nil
+  [list-of-stuff]
+  (def new-list (mapv #(treat %) list-of-stuff))
+  (println new-list)
+  (apply max new-list))
+
+(defn sort
+  [tree]
+  (if (= tree nil)
+    nil
+    (let [
+          left (sort (:left tree))
+          right (sort (:right tree))
+          ]
+      
+      (do 
+
+        (println "        \n")
+        (println "--------------------------")
+        (println (:value tree))
+        (println "LEFT IS: " left)
+
+        (println "        \n")
+        (println "--------------------------")
+        (println (:value tree))
+        (println "LEFT IS: " left)
+        (println "RIGHT IS: " right)
+
+        (def max-value (max-with-nil [(:value left) (:value right) (:value tree)]))
+        (cond
+          (= max-value (:value left))  {:value max-value :left (assoc left :value (:value tree)) :right right}
+          (= max-value (:value right)) {:value max-value :left left :right (assoc right :value (:value tree))}
+          (= max-value (:value tree))  {:value max-value :left left :right right})))))
 
 
 (defn -main
   [& args]
-  (create-tree [1 2 3 4 5])
+  (def tree (create-tree [1 2 3 4 5 99 1000 212 278]))
+  (println "Initial TREE")
+  (println tree)
+  (def sorted-tree (sort tree))
+  (println "FINAL TREE")
+  (println sorted-tree)
   )
 
